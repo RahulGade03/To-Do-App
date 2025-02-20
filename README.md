0b63b50779dbe111bf6f52b97e5ffcf1f5e6381d
RahulGade03 <rahulgade2004@gmail.com>
2025-02-20 19:34:06 +0530

    Added todo app

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# To-Do-App
-This To Do Application helps you track your daily tasks, it can also store your tasks on local storage.
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import { useState, useRef } from 'react'
+import './App.css'
+import { v4 as uuidv4 } from 'uuid';
+import { useEffect } from 'react';
+
+function App() {
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
+  const [toggle, setToggle] = useState(false)
+  const inputRef = useRef(null)
+
+  useEffect(() => {
+    let tempTasks = [...tasks]
+    if (tempTasks.length > 0){
+      localStorage.setItem("tasks", JSON.stringify(tempTasks))
+    }
+    console.log(JSON.parse(localStorage.getItem('tasks')))
+  }, [tasks])
+
+  useEffect(() => {
+    console.log(JSON.parse(localStorage.getItem('tasks')))
+    if (localStorage.getItem('tasks')) {
+      setTasks(JSON.parse(localStorage.getItem('tasks')))
+    }
+  },[])
+
+  function handleChange(e) {
+    setTask(e.target.value)
+  }
+
+  function handleAdd() {
+    setTasks([...tasks, { id: uuidv4(), task: task, isCompleted: false }])
+    setTask('')
+  }
+
+  function handleEdit(id) {
+    let tempTask = tasks.findIndex((task) => {
+      return task.id === id
+    })
+
+    setTask(tasks[tempTask].task)
+    let newTasks = tasks.filter((task) => {
+      return task.id !== id
+    })
+    setTasks(newTasks)
+  }
+
+  function handleDelete(id) {
+    let newTasks = tasks.filter((task) => {
+      return task.id !== id
+    })
+    console.log(newTasks)
+    setTasks(newTasks);
+  }
+
+  function handleCheckBox(e) {
+    let id = e.target.name;
+    let index = tasks.findIndex((task) => {
+      return task.id === id
+    })
+    let tempTasks = [...tasks]
+    tempTasks[index].isCompleted = !tempTasks[index].isCompleted
+    setTasks(tempTasks)
+  }
+
+  const Todo = ({ props }) => {
+    return ((toggle || !props.isCompleted) && (
+      <div key={props.id} className="flex justify-between items-center my-5">
+        <div className='flex gap-5'>
+        <input type="checkbox" name={props.id} onChange={handleCheckBox} className='cursor-pointer scale-150' />
+        <div className={props.isCompleted ? 'line-through' : ''}>{props.task}</div>
+        </div>
+        <div className='flex gap-5'>
+          <button className="bg-indigo-900 px-5 rounded-2xl text-white py-1" onClick={() => { handleEdit(props.id) }}>Edit</button>
+          <button className="bg-indigo-900 px-5 rounded-2xl text-white py-1" onClick={() => { handleDelete(props.id) }}>Delete</button>
+        </div>
+      </div>
+    ))
+  }
+
+
+  return (
+    <div className="flex justify-center items-start min-h-screen bg-indigo-300">
+      <div className="bg-indigo-400 min-w-500px min-h-[500px] px-10 rounded-2xl my-4">
+        <h1 className="text-3xl font-bold underline text-center py-5">
+          GoatTask - Your Task Planner
+        </h1>
+
+        <div className="container my-5">
+          <div className="insert flex justify-center gap-5 border-t-2 border-b-2 py-2">
+            <input type="text" placeholder='Enter your task...' className='bg-white text-black rounded-2xl p-4' value={task} onChange={handleChange} />
+            <button className='bg-indigo-900 px-10 rounded-2xl text-white' onClick={handleAdd}>Add</button>
+          </div>
+
+          <div className="tasks">
+            <label className='flex items-center gap-5 border-b-2'>
+              <input type="checkbox" checked={toggle} onClick={() => {setToggle(!toggle)}} ref={inputRef} className='scale-150'/>
+              <span className='text-xl'>Show Finished</span>
+            </label>
+            {tasks.map((item) => {
+              return (
+                <Todo props={item} />
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
